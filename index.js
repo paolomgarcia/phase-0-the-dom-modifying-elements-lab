21bc17e91d47b0892c0c6ac15147da1ea0b3536c
Paolo Garcia <pmatteo1108@yahoo.com>
2022-09-11 02:07:31 -0600

    commit tree 96a74226664ed4d11d878a03d08121665c9ea856

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+var newHeader = document.createElement('H1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
+
+
+element.remove('main');
+
